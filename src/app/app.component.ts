4d30642b4286f5f73386d434de38fed530b2ed19
m.dove@live.com <m.dove@live.com>
2020-01-05 23:08:40 -0800

    Added boilerplate template styling to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { WOW } from 'wowjs/dist/wow.min';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cutecryption';
+  title = 'cutecryption';  
+  constructor() {
+  }
+  ngAfterViewInit(){
+    new WOW().init();
+ }
 }
+
+
