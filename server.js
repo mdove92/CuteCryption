7e279059e6e0c13edbd1c2664dc7869ea5b5e7fb
m.dove@live.com <m.dove@live.com>
2020-01-07 14:21:42 -0800

    Have express reroute to correct directory

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const path = require('path');
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/cutecryption/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
