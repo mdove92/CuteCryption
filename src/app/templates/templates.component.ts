ace8d75b11f300822835f88cc7420ec8a506a7de
m.dove@live.com <m.dove@live.com>
2020-01-15 13:55:13 -0800

    added openpgp scripts for creation of public/private key

--- src/app/templates/templates.component.ts
+++ src/app/templates/templates.component.ts
@@ -2,13 +2,17 @@ import { Component, OnInit, Input } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import {FormControl, FormGroup, Validators, FormsModule} from '@angular/forms';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import openpgp from 'openpgp';
 
 @Component({
   selector: "app-templates",
   templateUrl: "./templates.component.html",
   styleUrls: ["./templates.component.css"]
 })
+
+
 export class TemplatesComponent implements OnInit {
+  
   javaBackendUrl = "https://cutecryption-backend.herokuapp.com/";
   templates = {};
   scope = {};
@@ -24,6 +28,7 @@ export class TemplatesComponent implements OnInit {
   @Input() selectedTemplate = "";
   @Input() safeHtmlTemplate;
   @Input() templateOutput = "";
+  @Input() password = "";
 
   constructor(private http: HttpClient, private sanitizer: DomSanitizer) {}
   formGroup = new FormGroup({
@@ -52,7 +57,21 @@ export class TemplatesComponent implements OnInit {
   onClickHandler = function(event) {
     this.templateStyle = "hidden";
     this.templateCreatedStyle = "shown";
+    
     this.bodyClass = "bodyGrown"
+
+    var options = {
+      userIds: [{  email:this.email }], // multiple user IDs
+      rsaBits: 4096,                                            // RSA key size
+      passphrase: this.password         // protects the private key
+    };
+
+    openpgp.generateKey(options).then(function(key) {
+      var privkey = key.privateKeyArmored; // '-----BEGIN PGP PRIVATE KEY BLOCK ... '
+      var pubkey = key.publicKeyArmored;   // '-----BEGIN PGP PUBLIC KEY BLOCK ... '
+      var revocationCertificate = key.revocationCertificate; // '-----BEGIN PGP PUBLIC KEY BLOCK ... '
+    });
+
     this.http.get(this.javaBackendUrl+`?templateName=${this.formStyle}&email=${this.email}`).subscribe(data => {
       this.safeHtmlTemplate =  this.sanitizer.bypassSecurityTrustHtml(
        this.templateOutput=  data[0]["Contents"]
